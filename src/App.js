eb3a06d56c8514b5b714227a79599091a09f46c6
punit-mistry <punitmistr@gmail.com>
2023-02-16 20:25:35 +0530

    First Commit of the chat app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {Box,Container,VStack,Button,Input, HStack} from '@chakra-ui/react'
+import Message from './Components/Message';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box bg={'lightcyan'}>
+      <Container h={'100vh'}bg={'lightcyan'}>
+        <VStack h={'100vh'} bg={'lightgrey'} padding={"6"}>
+          <Button colorScheme={"red"} w={'full'}>
+            SignOut
+          </Button>
+          <VStack h={"full"} w={'full'}>
+
+          <Message text={"this is the Sample Message For testing purpose"} />
+
+          </VStack>
+
+          <form style={{width:"100%",padding:"10px",backgroundColor:"lightblue"}}>
+           <HStack>
+           <Input type={"text"} w={"full"} placeholder={"Enter Your Message Here..."}></Input>
+            <Button colorScheme={"purple"} type={"submit"} >Send</Button>
+           </HStack>
+          </form>
+        </VStack>
+      </Container>
+    </Box>
   );
 }
 
